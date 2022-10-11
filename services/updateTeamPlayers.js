18ddc8955e8694f388d35153ecec5a81745b27f2
nhamonin <gamonin.nazar@gmail.com>
2022-10-11 10:49:50 +0300

    minor code refactoring

--- services/updateTeamPlayers.js
+++ services/updateTeamPlayers.js
@@ -7,12 +7,12 @@ export const updateTeamPlayers = async (chat_id) => {
     const team = await Team.findOne({ chat_id });
     const teamNicknames = await team
       .populate('players')
-      .then((teamModel) => teamModel.players.map(({ nickname }) => nickname));
+      .then(({ players }) => players.map(({ nickname }) => nickname));
 
     const playersStats = await getPlayersStats(teamNicknames);
 
-    playersStats.map(async ({ nickname, elo, lvl }) => {
-      await Player.findOneAndUpdate({ nickname }, { elo, lvl });
+    playersStats.map(({ nickname, elo, lvl }) => {
+      Player.findOneAndUpdate({ nickname }, { elo, lvl });
     });
 
     return messages.updateTeamPlayers.success;
