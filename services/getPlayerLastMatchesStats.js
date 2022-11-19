36218981f2defce0ddf0f6ff990266e520077dcf
nhamonin <gamonin.nazar@gmail.com>
2022-11-19 23:25:20 +0200

    prettified output for player's matches

--- services/getPlayerLastMatchesStats.js
+++ services/getPlayerLastMatchesStats.js
@@ -1,26 +1,50 @@
-import { Player, Team } from '../models/index.js';
+import { Team } from '../models/index.js';
 
 export const getPlayerLastMatchesStats = async (chat_id, playerNickname) => {
   const team = await Team.findOne({ chat_id });
   const [player] = (await team.populate('players'))?.players.filter(
     ({ nickname }) => nickname === playerNickname
   );
-  const playerMatches = await player
-    .populate('matches')
-    .then(({ matches }) => matches);
+  const playerMatches = (await player.populate('matches'))?.matches;
 
-  const message = formatMessage(playerMatches);
+  const message = formatMessage(playerMatches, player.player_id);
 
   return { message };
 };
 
-function formatMessage(playerMatches) {
-  return playerMatches
-    .map(
-      (match, index) =>
-        `${++index}: ${match.round_stats.Map.replace('de_', '')} - ${
-          match.round_stats.Score
-        }`
-    )
-    .join('\n');
+function formatMessage(playerMatches, playerID) {
+  return [
+    '<code>Result  Score  PlayerK/D  Map',
+    ...playerMatches.map((match) => {
+      const roundStats = match.round_stats;
+      const playerTeam =
+        match.teams[
+          match.teams.findIndex((team) =>
+            team.players.some(({ player_id }) => player_id === playerID)
+          )
+        ];
+      const result =
+        roundStats.Winner === playerTeam.team_id ? ' W 🟢' : ' L 🔴';
+      const score = roundStats.Score.split('/')
+        .map((teamScore, index) => {
+          const prefix = index === 0 ? ' ' : '';
+          teamScore = teamScore.trim();
+          return (
+            prefix + (teamScore.length === 1 ? teamScore + ' ' : teamScore)
+          );
+        })
+        .join('/');
+      const { player_stats } = playerTeam.players.find(
+        ({ player_id }) => player_id === playerID
+      );
+      const playerKD =
+        '  ' +
+        (+player_stats['K/D Ratio']).toFixed(2) +
+        (player_stats['K/D Ratio'] >= 1 ? ' 🟢' : ' 🔴');
+      const map = ' ' + roundStats.Map.replace('de_', '');
+
+      return `${result}  ${score}${playerKD} ${map}`;
+    }),
+    '</code>',
+  ].join('\n');
 }
