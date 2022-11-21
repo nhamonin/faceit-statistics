98baaad643b6203747262da92e79f6cc9d918131
nhamonin <gamonin.nazar@gmail.com>
2022-11-21 13:22:51 +0200

    fixed last matches indentation; added error logging for the last matches command

--- services/addPlayerToTeam.js
+++ services/addPlayerToTeam.js
@@ -3,7 +3,7 @@ import {
   getPlayersStats,
   storePlayerMatchesInDB,
 } from '../utils/index.js';
-import { Player, Team, Match } from '../models/index.js';
+import { Player, Team } from '../models/index.js';
 import { messages } from '../config/config.js';
 
 export const addPlayer = async (playerNickname, chat_id) => {
