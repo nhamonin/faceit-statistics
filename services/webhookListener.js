b44bb163f0f6dfc34e32f13ae0e88f107f5d7fe7
nhamonin <gamonin.nazar@gmail.com>
2022-12-31 12:02:00 +0200

    delete useless console.logs

--- services/webhookListener.js
+++ services/webhookListener.js
@@ -95,12 +95,10 @@ export function webhookListener() {
               allowedCompetitionName
             ) {
               clearInterval(interval);
-              console.log('clear');
 
               const predictions = await calculateBestMaps(matchData);
               if (predictions?.length) {
                 const prediction = await TempPrediction.findOne({ match_id });
-                console.log('pred');
 
                 if (!prediction) {
                   const newPrediction = new TempPrediction({
