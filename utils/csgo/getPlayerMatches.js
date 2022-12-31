7bafe3ea7b4e3b01df56f725761621ea746ddb5a
nhamonin <gamonin.nazar@gmail.com>
2022-12-31 17:21:44 +0200

    add logging for getPlayerLastMatches response

--- utils/csgo/getPlayerMatches.js
+++ utils/csgo/getPlayerMatches.js
@@ -4,6 +4,7 @@ export async function getPlayerMatches(player_id, amount = 20) {
   const url = `https://api.faceit.com/stats/v1/stats/time/users/${player_id}/games/csgo?page=0&size=${amount}`;
   const response = await fetch(url);
   const matches = await response.json();
+  console.log(JSON.stringify(matches));
 
   return matches;
 }
