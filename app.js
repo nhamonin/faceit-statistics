9aa0ee0ee5ce2c3084d01114ad6329f569c151a5
nhamonin <gamonin.nazar@gmail.com>
2023-01-02 15:19:20 +0200

    add 2 more Faceit API keys to avoid rate limiting

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import { Faceit } from 'faceit-node-api';
 
-import { FACEIT_API_KEY } from '#config';
+import { FACEIT_API_KEY_1, FACEIT_API_KEY_2, FACEIT_API_KEY_3 } from '#config';
 import { initTelegramBotListener } from '#controllers';
 import { webhookListener } from '#services';
 import { connectDB, adjustConsoleLog } from '#utils';
 
-Faceit.setApiKey(FACEIT_API_KEY);
+Faceit.setApiKeys([FACEIT_API_KEY_1, FACEIT_API_KEY_2, FACEIT_API_KEY_3]);
 
 await connectDB();
 adjustConsoleLog();
