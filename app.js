8a557d1112c030b52ab35dafc9b34d141e36ec17
nhamonin <gamonin.nazar@gmail.com>
2023-01-06 13:44:13 +0200

    added 5 keys, optimize export

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import { Faceit } from 'faceit-node-api';
 
-import { FACEIT_API_KEY_1, FACEIT_API_KEY_2, FACEIT_API_KEY_3 } from '#config';
+import { FACEIT_API_KEYS } from '#config';
 import { initTelegramBotListener } from '#controllers';
 import { webhookListener } from '#services';
 import { connectDB, adjustConsoleLog } from '#utils';
 
-Faceit.setApiKeys([FACEIT_API_KEY_1, FACEIT_API_KEY_2, FACEIT_API_KEY_3]);
+Faceit.setApiKeys(FACEIT_API_KEYS);
 
 await connectDB();
 adjustConsoleLog();
