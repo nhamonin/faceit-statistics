869784f3eb7de064b5842bd98fd08a8ce453e955
nhamonin <gamonin.nazar@gmail.com>
2023-01-20 20:05:10 +0200

    possible fix for mongo connect errors

--- utils/db/mongo.js
+++ utils/db/mongo.js
@@ -15,16 +15,29 @@ export default async function connectDB() {
       : MONGO_DB_CLUSTER_NAME_TEST;
   const uri = `mongodb+srv://${MONGO_DB_NAME}:${MONGO_DB_PASSWORD}@cluster0.cqna7jk.mongodb.net/${mongoCluster}?w=majority`;
 
-  await mongoose
-    .connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      maxIdleTimeMS: 30000,
-      maxConnecting: 10,
-      retryWrites: true,
-      socketTimeoutMS: 45000,
-      keepAlive: true,
-    })
-    .then(() => console.log('Connected to DB', new Date().toLocaleString()))
-    .catch((e) => console.log(e.message));
+  const mongooseConnectWrapper = async () => {
+    try {
+      await mongooseConnect();
+    } catch (e) {
+      await mongooseConnectWrapper();
+      console.log(e);
+    }
+  };
+
+  await mongooseConnectWrapper();
+
+  async function mongooseConnect() {
+    await mongoose
+      .connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        maxIdleTimeMS: 30000,
+        maxConnecting: 10,
+        retryWrites: true,
+        socketTimeoutMS: 45000,
+        keepAlive: true,
+      })
+      .then(() => console.log('Connected to DB', new Date().toLocaleString()))
+      .catch((e) => console.log(e.message));
+  }
 }
