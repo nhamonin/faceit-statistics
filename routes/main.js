6b95a208ba5ec008ecd591f162990e8db5a68baf
nhamonin <gamonin.nazar@gmail.com>
2023-01-28 01:20:29 +0200

    add express status monitor

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,11 @@
 import path from 'path';
 
 import express from 'express';
+import expressStatusMonitor from 'express-status-monitor';
 
 const router = express.Router();
 
+router.use(expressStatusMonitor());
 router.get('/', (req, res) => {
   res.sendFile(path.join(process.cwd(), 'public', 'templates', '/index.html'));
 });
