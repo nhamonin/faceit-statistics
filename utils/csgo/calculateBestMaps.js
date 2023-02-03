0cddcfb9b1f5e55e10259b99b870ac3b18890dcb
nhamonin <gamonin.nazar@gmail.com>
2023-02-03 22:13:58 +0200

    remove useless consoles

--- utils/csgo/calculateBestMaps.js
+++ utils/csgo/calculateBestMaps.js
@@ -244,9 +244,7 @@ async function sendMapPickerResult(
   team1Name,
   team2Name
 ) {
-  console.log('before');
   if (!!dbPlayersTeam1.length && !!dbPlayersTeam2.length) return;
-  console.log('after');
   try {
     const neededVariables = dbPlayersTeam1.length
       ? [dbPlayersTeam1, team1Result, team1Name]
