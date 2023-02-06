a6ff642f29e8fd4c267d2f268a83f7a796f24836
Nazarii Hamonin <nazarii.hamonin@gmail.com>
2023-02-06 19:15:09 +0200

    add no-cache headers

--- utils/csgo/getMatchData.js
+++ utils/csgo/getMatchData.js
@@ -3,7 +3,11 @@ import fetch from 'node-fetch';
 export async function getMatchData(match_id) {
   try {
     const url = `https://api.faceit.com/match/v2/match/${match_id}`;
-    const res = await fetch(url);
+    const res = await fetch(url, {headers: {
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      'Pragma': 'no-cache',
+      'Expires': 0
+    }});
     if (res.ok) {
       return res.json();
     } else {
