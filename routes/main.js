8eba0f8d3618daecea5279d340fe4977197f9f9a
nhamonin <gamonin.nazar@gmail.com>
2023-02-07 11:27:17 +0200

    remove express-status-monitor

--- routes/main.js
+++ routes/main.js
@@ -1,11 +1,9 @@
 import path from 'path';
 
 import express from 'express';
-import expressStatusMonitor from 'express-status-monitor';
 
 const router = express.Router();
 
-router.use(expressStatusMonitor());
 router.get('/', (req, res) => {
   res.sendFile(path.join(process.cwd(), 'public', 'templates', '/index.html'));
 });
