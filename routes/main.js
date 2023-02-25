79c861f83a5dcce37295916dbb06429615409aad
nhamonin <gamonin.nazar@gmail.com>
2023-02-26 00:37:37 +0200

    move index.html to the root folder of the project; add validation

--- routes/main.js
+++ routes/main.js
@@ -5,7 +5,16 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(process.cwd(), 'public', 'templates', '/index.html'));
+  res.sendFile(path.join(process.cwd(), '/index.html'));
 });
+
+router.get(
+  '/.well-known/pki-validation/62479386646DE7D4241078C70CDC23AD.txt',
+  (req, res) => {
+    res.sendFile(
+      path.join(process.cwd(), '/62479386646DE7D4241078C70CDC23AD.txt')
+    );
+  }
+);
 
 export default router;
