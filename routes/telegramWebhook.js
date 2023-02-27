f4937ed2815f4b38210c6b0b119a6c6fda143808
nhamonin <gamonin.nazar@gmail.com>
2023-02-27 02:56:00 +0200

    add processUpdate function when receiving webhook

--- routes/telegramWebhook.js
+++ routes/telegramWebhook.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
-import { TELEGRAM_BOT_API_TOKEN } from '#config';
+import { TELEGRAM_BOT_API_TOKEN, bots } from '#config';
 
 const router = express.Router();
 
 router.post(`/telegram-webhook-${TELEGRAM_BOT_API_TOKEN}`, async (req, res) => {
-  console.log(JSON.stringify(req.body));
+  const update = req.body;
+
+  bots.telegram.processUpdate(update);
 
   res.sendStatus(200);
 });
