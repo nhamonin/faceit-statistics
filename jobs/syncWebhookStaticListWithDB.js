9d2587441d1fd7f6f1a1f6b39cc18ae73e9bb934
Nazarii Hamonin <nazarii.hamonin@gmail.com>
2023-03-15 19:23:08 +0200

    fix bug in sync db job

--- jobs/syncWebhookStaticListWithDB.js
+++ jobs/syncWebhookStaticListWithDB.js
@@ -1,9 +1,9 @@
 import { Player } from '#models';
-import { webhookMgr } from '#utils';
+import { webhookMgr, chunk } from '#utils';
 
 export async function syncWebhookStaticListWithDB() {
   try {
-    const playersIDs = await Player.find().select('player_id').lean();
+    const playersIDs = (await Player.find().select('player_id -_id').lean()).map(({ player_id }) => player_id);
     const { restrictions } = await webhookMgr.getWebhookDataPayload();
 
     const playersIDsNotInWebHook = playersIDs.filter((player_id) => {
@@ -14,7 +14,11 @@ export async function syncWebhookStaticListWithDB() {
     });
 
     if (playersIDsNotInWebHook.length) {
-      await webhookMgr.addPlayersToList(playersIDsNotInWebHook);
+      const playersIDsNotInWebHookChunks = chunk(playersIDsNotInWebHook, 5);
+
+      for await (const playersChunked of playersIDsNotInWebHookChunks) {
+        await webhookMgr.addPlayersToList(playersChunked);
+      }
     }
   } catch (e) {
     console.log(e.message);
