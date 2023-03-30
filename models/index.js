59ee5fe799f52011a12b32835a69833f44b1acc1
nhamonin <gamonin.nazar@gmail.com>
2023-03-30 23:21:20 +0300

    remove models and redundant files after db migration

--- models/index.js
+++
@@ -1,6 +0,0 @@
-import { Player } from './player.js';
-import { Team } from './team.js';
-import { MatchPrediction } from './matchPrediction.js';
-import { TempPrediction } from './tempPrediction.js';
-
-export { Player, Team, MatchPrediction, TempPrediction };
