174b758c4f9569614ab88038a2187f584edc748a
nhamonin <gamonin.nazar@gmail.com>
2023-04-05 14:35:52 +0300

    add getting webhook token on app start + every 10 min via node-cron

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ import { Faceit } from 'faceit-node-api';
 
 import { FACEIT_API_KEY } from '#config';
 import { initTelegramBotListener } from '#controllers';
-import { adjustConsoleLog, startExpressServer, initI18next } from '#utils';
+import {
+  adjustConsoleLog,
+  startExpressServer,
+  initI18next,
+  webhookMgr,
+} from '#utils';
 import { runCrons } from '#jobs';
 
 Faceit.setApiKeys([FACEIT_API_KEY]);
@@ -13,3 +18,4 @@ adjustConsoleLog();
 startExpressServer();
 runCrons();
 await initI18next();
+await webhookMgr.getWebhookDataPayload();
--- jobs/runCrons.js
+++ jobs/runCrons.js
@@ -1,8 +1,10 @@
 import cron from 'node-cron';
 
 import { syncWebhookStaticListWithDB, deleteOldTempPredictions } from '#jobs';
+import { webhookMgr } from '#utils';
 
 export function runCrons() {
   cron.schedule('0 4 * * *', syncWebhookStaticListWithDB);
   cron.schedule('0 * * * *', deleteOldTempPredictions);
+  cron.schedule('*/10 * * * *', webhookMgr.getWebhookDataPayload);
 }
