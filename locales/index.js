8365a8c2179046f0ff7891369632dcbb24a049a5
nhamonin <gamonin.nazar@gmail.com>
2023-04-06 14:14:24 +0300

    remove usage of experimental feature -  assert { type: 'json' }

--- locales/index.js
+++ locales/index.js
@@ -1,4 +1,23 @@
-import en from './en/en.json' assert { type: 'json' };
-import uk from './uk/uk.json' assert { type: 'json' };
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+async function loadJsonFile(filePath) {
+  const dirname = fileURLToPath(new URL('.', import.meta.url));
+  const absolutePath = path.join(dirname, filePath);
+  const fileContent = await fs.readFile(absolutePath, 'utf-8');
+  return JSON.parse(fileContent);
+}
+
+async function loadTranslations() {
+  const [en, uk] = await Promise.all([
+    loadJsonFile('./en/en.json'),
+    loadJsonFile('./uk/uk.json'),
+  ]);
+
+  return { en, uk };
+}
+
+const { en, uk } = await loadTranslations();
 
 export { en, uk };
