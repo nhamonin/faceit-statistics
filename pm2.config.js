65f1fc86784e4e08665c9929c8e08675cfe406fa
nhamonin <gamonin.nazar@gmail.com>
2023-04-06 13:52:17 +0300

    rewrite pm2.config.js to common js

--- pm2.config.js
+++ pm2.config.js
@@ -1,9 +1,10 @@
-export default {
+module.exports = {
   apps: [
     {
       name: 'app',
       script: 'app.js',
       interpreter: '/root/.nvm/versions/node/v19.3.0/bin/node',
+      node_args: '--experimental-specifier-resolution=node',
     },
   ],
 };
