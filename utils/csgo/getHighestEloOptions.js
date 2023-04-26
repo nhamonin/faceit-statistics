84b24d7986c2b977651be8522f8c903e4e6f822a
nhamonin <gamonin.nazar@gmail.com>
2023-04-26 12:55:20 +0300

    Refactor getHighestEloOptions to return the highest elo and its timestamp

--- utils/csgo/getHighestEloOptions.js
+++ utils/csgo/getHighestEloOptions.js
@@ -13,6 +13,6 @@ export async function getHighestEloOptions(player_id) {
 
   return {
     elo: highestEloMatch?.elo,
-    date: highestEloMatch?.date,
+    date: highestEloMatch?.timestamp,
   };
 }
