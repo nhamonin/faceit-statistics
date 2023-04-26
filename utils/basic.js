80d832e60c3a286264270caa1aaf3f1246735215
nhamonin <gamonin.nazar@gmail.com>
2023-04-26 12:09:54 +0300

    Fix console log to console error in withErrorHandling function in basic.js file

--- utils/basic.js
+++ utils/basic.js
@@ -218,7 +218,7 @@ function withErrorHandling(fn, errorObj) {
     try {
       return await fn(...args);
     } catch (e) {
-      console.log(e);
+      console.error(e);
       return {
         error: errorObj?.error,
         text: errorObj?.errorMessage,
