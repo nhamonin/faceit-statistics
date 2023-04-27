ae04fcb120d3d6aac8748c354c125e50b66f8141
nhamonin <gamonin.nazar@gmail.com>
2023-04-27 09:53:05 +0300

    fixed minor bug

--- jobs/index.js
+++ jobs/index.js
@@ -1,6 +1,6 @@
 import { syncWebhookStaticListWithDB } from './scheduled/syncWebhookStaticListWithDB.js';
 import { deleteOldTempPredictions } from './scheduled/deleteOldTempPredictions.js';
-import { performUpdatePlayers } from './scheduled/updatePlayers';
+import { performUpdatePlayers } from './scheduled/updatePlayers.js';
 
 import { runCrons } from './runCrons.js';
 
