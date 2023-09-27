4e12d2bfb0b3e9c2250cd98620952cb8a84867d9
nhamonin <gamonin.nazar@gmail.com>
2023-09-28 00:52:37 +0300

    Remove data from routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 import main from './main.js';
 import faceitWebhook from './faceitWebhook.js';
 import telegramWebhook from './telegramWebhook.js';
-import data from './data.js';
 
-export default { ...main, ...faceitWebhook, ...telegramWebhook, ...data };
+export default { ...main, ...faceitWebhook, ...telegramWebhook };
