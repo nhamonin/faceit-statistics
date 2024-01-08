1e9abfa1d550cbf6ab44254171bf0c482be4877a
nhamonin <gamonin.nazar@gmail.com>
2024-01-08 22:37:55 +0200

    Add max_size to pm2.config.cjs

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
       script: 'app.js',
       interpreter: '/root/.nvm/versions/node/v19.3.0/bin/node',
       node_args: '--experimental-specifier-resolution=node',
+      max_size: '100M',
     },
   ],
 };
