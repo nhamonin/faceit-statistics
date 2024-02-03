990986b9b51503bb37fcfb44d55628e505ff9bac
nhamonin <gamonin.nazar@gmail.com>
2024-02-03 21:18:44 +0200

    Remove interpreter version from pm2.config.cjs

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -3,7 +3,6 @@ module.exports = {
     {
       name: 'app',
       script: 'app.js',
-      interpreter: 'node@20.11.0',
       node_args: '--experimental-specifier-resolution=node --env-file=.env',
       max_size: '100M',
     },
