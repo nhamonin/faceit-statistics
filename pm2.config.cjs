cf3135e0e4b2be16a2530490149bb92e6be31058
nhamonin <gamonin.nazar@gmail.com>
2024-02-03 21:06:32 +0200

    Add interpreter version to pm2.config.cjs

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
     {
       name: 'app',
       script: 'app.js',
+      interpreter: 'node@20.11.0',
       node_args: '--experimental-specifier-resolution=node --env-file=.env',
       max_size: '100M',
     },
