409115cbe6a82a641e23fad857d5a8791e683e9d
nhamonin <gamonin.nazar@gmail.com>
2024-02-26 11:36:46 +0200

    ssl validation

--- routes/main.js
+++ routes/main.js
@@ -17,4 +17,19 @@ export default {
       });
     },
   },
+  '/.well-known/pki-validation/62479386646DE7D4241078C70CDC23AD.txt': {
+    get: (req, res) => {
+      const filePath = path.join(process.cwd(), '/0-62479386646DE7D4241078C70CDC23AD.txt');
+
+      fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Internal Server Error');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(data);
+        }
+      });
+    },
+  },
 };
