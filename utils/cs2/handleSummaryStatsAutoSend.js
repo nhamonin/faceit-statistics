b16af92e29139068910be8e9c877342695b13b47
nhamonin <gamonin.nazar@gmail.com>
2024-06-03 21:29:46 +0300

    Add waiting mechanism for handleSummaryStatsAutoSend

--- utils/cs2/handleSummaryStatsAutoSend.js
+++ utils/cs2/handleSummaryStatsAutoSend.js
@@ -5,53 +5,44 @@ import { getSummaryStatsTemplate } from '#templates';
 import { subscriptionReceivedMarkup } from '#telegramReplyMarkup';
 import { caches } from '#config';
 
-export async function handleSummaryStatsAutoSend(
-  matchID,
-  chatIDs,
-  playersWithResults
-) {
-  const addedToCache = cacheWithExpiry(
-    caches.summaryStatsMatchIDs,
-    matchID,
-    1000 * 60 * 3
-  );
+export async function handleSummaryStatsAutoSend(matchID, chatIDs, playersWithResults) {
+  const addedToCache = cacheWithExpiry(caches.summaryStatsMatchIDs, matchID, 1000 * 60 * 3);
   if (!addedToCache) return;
 
-  chatIDs.map(async (chat_id) => {
-    const team = await database.teams.readBy({ chat_id });
-    const statusFinishedSubscriptions =
-      team.settings.subscriptions.match_status_finished;
-    if (!statusFinishedSubscriptions.summaryStats) return;
-    const teamPlayers = await database.players.readAllByChatId(chat_id, [
-      'nickname',
-      'player_id',
-    ]);
-    const playedPlayers = teamPlayers
-      .filter((player) =>
-        playersWithResults.map(({ id }) => id).includes(player.player_id)
-      )
-      .map(({ nickname }) => nickname);
-    const { errorMessage, data } = await getSummaryStats(
-      chat_id,
-      playedPlayers,
-      playersWithResults
-    );
-    errorMessage
-      ? await telegramSendMessage(chat_id, { text: errorMessage })
-      : await sendPhoto([chat_id], null, getSummaryStatsTemplate(data));
-    await telegramSendMessage(
-      chat_id,
-      {
-        text: 'subscriptions.summaryStats.message',
-        options: {
-          teamNicknames: playedPlayers.join(', '),
-          count: playedPlayers.length,
+  await Promise.all(
+    chatIDs.map(async (chat_id) => {
+      const team = await database.teams.readBy({ chat_id });
+      const statusFinishedSubscriptions = team.settings.subscriptions.match_status_finished;
+      if (!statusFinishedSubscriptions.summaryStats) return;
+      const teamPlayers = await database.players.readAllByChatId(chat_id, [
+        'nickname',
+        'player_id',
+      ]);
+      const playedPlayers = teamPlayers
+        .filter((player) => playersWithResults.map(({ id }) => id).includes(player.player_id))
+        .map(({ nickname }) => nickname);
+      const { errorMessage, data } = await getSummaryStats(
+        chat_id,
+        playedPlayers,
+        playersWithResults
+      );
+      errorMessage
+        ? await telegramSendMessage(chat_id, { text: errorMessage })
+        : await sendPhoto([chat_id], null, getSummaryStatsTemplate(data));
+      await telegramSendMessage(
+        chat_id,
+        {
+          text: 'subscriptions.summaryStats.message',
+          options: {
+            teamNicknames: playedPlayers.join(', '),
+            count: playedPlayers.length,
+          },
         },
-      },
-      {
-        parse_mode: 'html',
-        ...subscriptionReceivedMarkup('match_status_finished', 'summaryStats'),
-      }
-    );
-  });
+        {
+          parse_mode: 'html',
+          ...subscriptionReceivedMarkup('match_status_finished', 'summaryStats'),
+        }
+      );
+    })
+  );
 }
