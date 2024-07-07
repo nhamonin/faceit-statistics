9546b1ecddd9fa7561e8fc96fbff72280775601c
nhamonin <gamonin.nazar@gmail.com>
2024-07-08 01:11:56 +0300

    Add yellow state to match predictions if value is pretty close to 0 (+-20)

--- utils/cs2/prettifyMapPickerData.js
+++ utils/cs2/prettifyMapPickerData.js
@@ -1,21 +1,37 @@
 export function prettifyMapPickerData(neededVariables) {
-  const table = `<table cellspacing="0">
-    <tr>
-      <th>Map</th>
-      <th>Impact Difference</th>
-      <th>Win Rate Difference, %</th>
-    </tr>
-    ${neededVariables[1]
-      .map(
-        ({ mapName, totalWinrate, totalPoints }) =>
-       `<tr class="${totalPoints > 0 ? 'green' : 'red'}">
-          <td>${mapName}</td>
-          <td>${totalPoints}</td>
-          <td>${totalWinrate}</td>
-        </tr>`
-      )
-      .join('')}
-    </table>`;
+  const getClassForPoints = (totalPoints) => {
+    if (totalPoints > 0) {
+      return 'green';
+    } else if (Math.abs(totalPoints) < 20) {
+      return 'yellow';
+    } else {
+      return 'red';
+    }
+  };
+
+  const createRow = ({ mapName, totalWinrate, totalPoints }) => {
+    const rowClass = getClassForPoints(totalPoints);
+    return `
+      <tr class="${rowClass}">
+        <td>${mapName}</td>
+        <td>${totalPoints}</td>
+        <td>${totalWinrate}</td>
+      </tr>
+    `;
+  };
+
+  const tableRows = neededVariables[1].map(createRow).join('');
+
+  const table = `
+    <table cellspacing="0">
+      <tr>
+        <th>Map</th>
+        <th>Impact Difference</th>
+        <th>Win Rate Difference, %</th>
+      </tr>
+      ${tableRows}
+    </table>
+  `;
 
   return table;
 }
