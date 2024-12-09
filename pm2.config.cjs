aff1a7c96217d463fff444e32fe32fad1eb04c84
nhamonin <gamonin.nazar@gmail.com>
2024-12-09 23:00:39 +0200

    Add interpreter option to pm2 config

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
       script: 'app.js',
       node_args: '--experimental-specifier-resolution=node --env-file=.env',
       max_size: '100M',
+      interpreter: 'node@21.6.1',
     },
   ],
 };
